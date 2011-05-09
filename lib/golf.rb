8c944827b66bebd9a822d3981934e3bbe751e95e
Sven Kräuter | 5v3n <sven.kraeuter@gmx.net>
2011-05-09 19:30:42 +0100

    intermediate commit for halfbyte merging action

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,20 @@
 class Golf
+  def Golf::hole1 a
+   a.inject{ |m,e| m*e }
+  end
+  def Golf::hole7 a
+    a
+  end
+  def Golf::hole8 n
+   r=[]
+   (1..n).each { |n| r << f(n) }
+   r
+  end
+  def Golf::f n
+    if n == 0 || n == 1
+      n
+    else
+      f(n-1) + f(n-2)
+    end
+  end
 end
