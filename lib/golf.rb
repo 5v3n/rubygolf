c1a6798e881cefe305ca0eaccb25f50127b96cfd
Jan Krutisch <jan@krutisch.de>
2011-05-09 19:24:22 +0100

    hole 4 and 5

--- lib/golf.rb
+++ lib/golf.rb
@@ -6,5 +6,19 @@ class Golf
   def self.hole2(s)
     s.split(" ").sort_by{|w| w[1,1]}.join(" ")
   end
-    
+  
+  def self.hole4(a)
+    a.map{|s|
+      s.gsub(/man\(.*\)/, 'hat(\0)').gsub(/dog\((.*?)\)/, 'dog(\1(bone))').gsub(/cat/, 'dead')
+    }
+  end
+  def self.hole5(a)
+    o = []
+    a.length.times do |i|
+      parts = a.length - i
+      o += parts.times.map{|j| a[j,i+1]}
+    end
+    o
+  end
 end
+  
